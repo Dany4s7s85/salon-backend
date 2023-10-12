d9c2b0b3c885e1a5fc8b9440e952c358ca657ee2
Dany4s7s85 <79710542+Dany4s7s85@users.noreply.github.com>
2023-10-12 15:42:06 +0500

    initial commit

---
+++ app.js
@@ -0,0 +1,15 @@
+// const myString = "Muhammad Danial Kamboh";
+// const obj = {};
+// const data = myString
+//   .toLowerCase()
+//   .split(" ")
+//   .join("")
+//   .split("")
+//   .map((e, i) => {
+//     if (obj[e]) {
+//       obj[e] += 1;
+//     } else {
+//       obj[e] = 1;
+//     }
+//   });
+// console.log(obj);
